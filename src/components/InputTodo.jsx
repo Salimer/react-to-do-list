21910d3f0849156495c125adfb32b6d428b9abd5
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-05 16:07:07 +0300

    Update InputTodo.jsx

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const InputTodo = ({ addTodoItem }) => {
     const [title, setTitle] = useState("");
+    const [message, setMessage] = useState("");
 
     const handleChange = (e) => {
         setTitle(e.target.value);
@@ -12,15 +13,19 @@ const InputTodo = ({ addTodoItem }) => {
         if (title.trim()) {
             addTodoItem(title);
             setTitle('');
+            setMessage('');
           } else {
-            alert('Please add item');
+            setMessage('Please write item');
           }
       };
     return (
-        <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Add Todo..." value={title} onChange={handleChange} />
-      <button>Submit</button>
+        <>
+        <form onSubmit={handleSubmit} className="form-container" >
+      <input className="input-text" type="text" placeholder="Add Todo..." value={title} onChange={handleChange} />
+      <button className="input-submit">Submit</button>
     </form>
+    <span className="submit-warning">{message}</span>
+        </>
     )
   };
   export default InputTodo;
