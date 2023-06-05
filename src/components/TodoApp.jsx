9e72d7f67f87de9aa81ae4bf0bbedd18d33d1b92
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-05 13:43:46 +0300

    Create TodoApp.jsx

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,10 @@
+const TodoApp = () => {
+    return (
+      <div>
+        <h1>Hello world!</h1>
+        <p>I am in a React Component!</p>
+      </div>
+    );
+  };
+  export default TodoApp;
+  
