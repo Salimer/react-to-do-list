12f386d1f62b944fc280e478f693ddf38fc6da8a
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-05 16:06:56 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import TodoApp from '@/components/TodoApp';
+import '@/styles/app.css';
 
 const domContainer = document.getElementById("root");
 const root = ReactDOM.createRoot(domContainer);
