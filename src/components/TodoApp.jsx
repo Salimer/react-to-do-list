5c9be3fe9e83cc73b38323bb74ec8a92a3565395
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-05 13:58:28 +0300

    Update TodoApp.jsx

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,10 +1,12 @@
+import Header from '@/components/Header';
+import TodosLogic from '@/components/TodosLogic';
+
 const TodoApp = () => {
-    return (
-      <div>
-        <h1>Hello world!</h1>
-        <p>I am in a React Component!</p>
-      </div>
-    );
-  };
-  export default TodoApp;
-  
+  return (
+    <>
+      <Header />
+      <TodosLogic />
+    </>
+  );
+};
+export default TodoApp;
